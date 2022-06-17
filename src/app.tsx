639ca79731389dac4aeb243616342f7f5387780d
Matheus Godoy <matheus.godoy@webjump.com.br>
2022-06-17 19:09:14 -0300

    feat: change app folder to pages

--- src/app.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import Routes from "./presentation/components/Routes/routes";
-import "./presentation/styles/globals.css";
-
-function App() {
-  return <Routes />;
-}
-export default App;
---
+++ src/pages/app.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import Routes from "../presentation/components/Routes/routes";
+import "../presentation/styles/globals.css";
+
+function App() {
+  return <Routes />;
+}
+export default App;

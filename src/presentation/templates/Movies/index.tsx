6fb16a8ad50d98c2b1428eb43bbcd022b83130c6
Matheus Godoy <matheus.godoy@webjump.com.br>
2022-06-19 13:59:49 -0300

    feat: add placeholder prop

--- src/presentation/templates/Movies/index.tsx
+++ src/presentation/templates/Movies/index.tsx
@@ -35,6 +35,7 @@ export default function MoviesTemplate() {
           <AutoComplete
             setMovie={setSelectedMovie}
             suggestions={moviesList}
+            placeholder="Movie"
           ></AutoComplete>
           <Text>{selectedMovie}</Text>
         </main>
